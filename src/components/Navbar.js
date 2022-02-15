51d196fc1075b65e409b45cbc79dfa6c53ec5c25
Edward Binion <eddiebinion2@gmail.com>
2022-02-15 09:04:45 -0600

    Backend Setup, Routes Working & React App Started

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+
+
+function Navbar() {
+  const [click, setClick] = useState(false);
+  return(
+    <>
+      <h1>Hello World</h1>
+    </>
+  )
+}
+
+export default Navbar;
